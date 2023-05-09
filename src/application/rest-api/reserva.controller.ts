5327df45b885f202edddd5402ee58f1d9bdbc0a4
Gabriel Gioscia Velloso <51892326+ggvell@users.noreply.github.com>
2023-05-09 01:17:43 +0000

    reserva.controller

--- src/application/rest-api/reserva.controller.ts
+++ src/application/rest-api/reserva.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Get, Inject, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Inject, Param, Post, Put, Query } from "@nestjs/common";
 import { GetReservaUsecase } from "src/core/domain/reserva/usecase/get-reserva.usecase";
 import { ReservaDto } from "src/core/service/dto/reserva.dto";
 import { ReservaRepositoryPortDI } from "../di/reserva.token";
+import { UpdateReservaUsecase } from "src/core/domain/reserva/usecase/update-reserva.usecase";
+import { DeleteReservaUsecase } from "src/core/domain/reserva/usecase/delete-reserva.usecase";
+import { PostReservaUsecase } from "src/core/domain/reserva/usecase/post-reserva.usecase";
+
 @Controller()
 export class ReservaController {
   constructor(
     @Inject(ReservaRepositoryPortDI)
     private readonly getReservaUsecase: GetReservaUsecase,
+    private readonly updateReservaUseCase: UpdateReservaUsecase,
+    private readonly postReservaUseCase: PostReservaUsecase,
+    private readonly deleteReservaUseCase: DeleteReservaUsecase,
   ) {}
 
-  @Get()
-  getReserva(@Query("id") id: number): ReservaDto {
+  @Get(':id')
+  getReserva(@Query("id") id: number): Promise<ReservaDto> {
     return this.getReservaUsecase.execute(id);
+  }
+
+  @Post()
+  postReserva(@Body() ReservaDto: ReservaDto): Promise<ReservaDto>{
+    return this.postReservaUseCase.execute(ReservaDto);
+  }
+
+  @Put(':id')
+  updateReserva(@Param("id") id: number, @Body() ReservaDto: ReservaDto): Promise<ReservaDto>{
+    return  this.updateReservaUseCase.execute(id, ReservaDto);
+  }
+
+  @Delete(':id')
+  deleteReserva(@Param("id") id : number): Promise<void> {
+    return this.deleteReservaUseCase.execute(id);
   }
 }
